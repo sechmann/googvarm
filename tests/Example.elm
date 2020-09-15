7771141fadf2886a427b34f770af5a9e86b9bbc6
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-09-15 18:06:20 +0200

    add json encode/decode test

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,6 +1,9 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
+import Json.Decode exposing (Decoder)
+import Json.Encode
+import Product exposing (Product)
 import Route
 import Test exposing (..)
 import Url
@@ -23,4 +26,33 @@ suite =
                         Nothing ->
                             Expect.fail "invalid url in test, get it together"
             ]
+        , describe "JSON encode/decode"
+            (let
+                product =
+                    { name = "Skinnfell", price = "10mnok,- / km^2", description = "description" }
+
+                productString =
+                    """{
+  "name": "Skinnfell",
+  "price": "10mnok,- / km^2",
+  "description": "description"
+}"""
+
+                decodedProduct =
+                    Json.Decode.decodeString Product.decoder productString
+
+                encodedProduct =
+                    Product.encode product
+             in
+             [ test "decode product" <|
+                \_ ->
+                    Expect.equal decodedProduct (Ok product)
+             , test "encode product" <|
+                \_ ->
+                    Expect.equal (Json.Encode.encode 2 encodedProduct) productString
+             , test "encode/decode roundtrip" <|
+                \_ ->
+                    Expect.equal (Ok product) (Json.Decode.decodeValue Product.decoder (Product.encode product))
+             ]
+            )
         ]
